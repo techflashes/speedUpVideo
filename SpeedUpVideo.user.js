0e262f73f3ea2452b5848b2e784c6bfed6d0cb85
techflashes <70425017+techflashes@users.noreply.github.com>
2020-09-04 14:08:56 -0400

    Create SpeedUpVideo.user.js

---
+++ SpeedUpVideo.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Speed up video
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Techflashes
+// @match        https://www.youtube.com/watch?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var _0x3938=['playbackRate','which','ctrlKey','html5-main-video','Speed?','altKey','onkeyup','getElementsByClassName'];(function(_0x262f74,_0x393881){var _0xcc3eb4=function(_0x7b104a){while(--_0x7b104a){_0x262f74['push'](_0x262f74['shift']());}};_0xcc3eb4(++_0x393881);}(_0x3938,0x104));var _0xcc3e=function(_0x262f74,_0x393881){_0x262f74=_0x262f74-0x0;var _0xcc3eb4=_0x3938[_0x262f74];return _0xcc3eb4;};var s=0x1;document[_0xcc3e('0x2')]=function(_0x7b104a){_0x7b104a[_0xcc3e('0x6')]&&_0x7b104a[_0xcc3e('0x1')]&&_0x7b104a[_0xcc3e('0x5')]==0x5a&&(s=prompt(_0xcc3e('0x0')),s!==null&&(document[_0xcc3e('0x3')](_0xcc3e('0x7'))[0x0][_0xcc3e('0x4')]=s));};
+})();
